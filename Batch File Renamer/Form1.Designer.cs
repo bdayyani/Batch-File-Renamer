e34c1abd086b0fe7dd5dfc06e31ea7f3e4108351
Babak Dayyani <babak.dayyani@gmail.com>
2020-07-14 16:01:35 -0400

    Add project files.

---
+++ Batch File Renamer/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Batch_File_Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button_load = new MetroFramework.Controls.MetroButton();
+            this.button_export = new MetroFramework.Controls.MetroButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(185, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(61, 22);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(79, 23);
+            this.button_load.TabIndex = 1;
+            this.button_load.Text = "Load Files";
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // button_export
+            // 
+            this.button_export.Location = new System.Drawing.Point(6, 83);
+            this.button_export.Name = "button_export";
+            this.button_export.Size = new System.Drawing.Size(79, 23);
+            this.button_export.TabIndex = 1;
+            this.button_export.Text = "Export";
+            this.button_export.Click += new System.EventHandler(this.button_export_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "New File Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button_load);
+            this.groupBox1.Location = new System.Drawing.Point(23, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 250);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Import";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.metroButton1);
+            this.groupBox2.Controls.Add(this.button_export);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(23, 319);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(197, 112);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Export Options";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(112, 83);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(79, 23);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "Open";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 453);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "File Renamer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton button_load;
+        private MetroFramework.Controls.MetroButton button_export;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ Batch File Renamer/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using MetroFramework;
+
+namespace Batch_File_Renamer
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        public string Source_Folder = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Open all the thermal images
+                listBox1.Items.Clear();
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.Description = "Open the folder containing Thermal images";
+                if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    Source_Folder = fbd.SelectedPath;
+                    string[] files = Directory.GetFiles(Source_Folder);
+                    foreach (string file in files)
+                    {
+                        try
+                        {
+                            string name = file.Replace((Source_Folder + "\\"), "");
+                            listBox1.Items.Add(name);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+
+                    }
+                }
+                listBox1.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\r\r" + ex.Message);
+            }
+        }
+
+        private void button_export_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:\r\r" + ex.Message);
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = Source_Folder,
+                UseShellExecute = true,
+                Verb = "open"
+            });
+        }
+    }
+}
